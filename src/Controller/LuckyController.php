5f9661a6c03f6a3b4681a72ffcf381e575e1858d
Ahmed Mihoub <mihoubahmed@live.Fr>
2018-04-25 12:34:55 +0100

    Adding LucyController

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of LuckyController
+ *
+ * @author Ahmed
+ */
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
